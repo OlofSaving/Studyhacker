631abb532767d3bcc94480b9c20961789085f26e
OlofSaving <olof.saving.1490@student.uu.se>
2026-05-23 12:08:48 +0200

    fixat calender bugg

--- frontend/script-API/calendar_API.js
+++ frontend/script-API/calendar_API.js
@@ -2,7 +2,7 @@
 console.log("calendar_API.js loaded");
 
 export async function getDeadlineTask(date) {
-    const response = await fetch(`/tasks/${date}`);
+    const response = await fetch(`/calendar/${date}`);
 
     if (!response.ok) {
         console.error("API error:", response.status);
